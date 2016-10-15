4a880ab91dd60527adab546fd83bfcd5b0c1e1d5
Brad Boehmke <bradleyboehmke@gmail.com>
2016-10-15 08:29:13 -0400

    initial commit

---
+++ stock-code.R
@@ -0,0 +1,22 @@
+# Packages used - run the next two lines of code if you do not have these
+# packages installed on your computer
+# pkgs <- c("xts", "quantmod")
+# install.packages(pkgs)
+library(xts)
+library(quantmod)
+
+# load data
+prices <- getSymbols("GOOG", auto.assign = FALSE)
+
+# get the most recent opening & closing prices
+move <- Cl(last(prices)) - Op(last(prices))
+
+# determine if you buy or sell
+ifelse(move > 0, "BUY", "SELL")
+
+# plot historical stock prices
+chartSeries(prices, theme = chartTheme("white", bg.col = "white"))
+
+# show stock info for last 7 days
+tail(prices, 7)[, 1:5]
+
